5c89bc189f96363fac5cce90804c7785474f4941
rbmkp4800 <rbmkp4800@gmail.com>
2015-11-30 15:55:57 +0200

    1.1

--- Source/XLib.Containers.Vector.h
+++ Source/XLib.Containers.Vector.h
@@ -10,7 +10,7 @@ class Vector : public NonCopyable
 {
 private:
 	HeapBuffer<Type> buffer;
-	uint32 _size;
+	uint32 bufferSize;
 
 	inline void expandBuffer(uint32 minNewSize)
 	{
@@ -26,40 +26,40 @@ private:
 	}
 
 public:
-	explicit Vector(uint32 initSize = 0) : buffer(initSize), _size(initSize) {}
-	Vector(uint32 initSize, const Type* initData) : buffer(initSize, initData), _size(initSize) {}
-	~Vector() { _size = 0; }
+	explicit Vector(uint32 initSize = 0) : buffer(initSize), bufferSize(initSize) {}
+	Vector(uint32 initSize, const Type* initData) : buffer(initSize, initData), bufferSize(initSize) {}
+	~Vector() { bufferSize = 0; }
 
-	Vector(Vector&& that) : buffer(move(that.buffer)), _size(that._size)
+	Vector(Vector&& that) : buffer(move(that.buffer)), bufferSize(that.bufferSize)
 	{
-		that._size = 0;
+		that.bufferSize = 0;
 	}
 	Vector& operator = (Vector&& that)
 	{
 		swap(buffer, that.buffer);
-		swap(_size, that._size);
+		swap(bufferSize, that.bufferSize);
 		return *this;
 	}
 
 	inline Type& pushBack(const Type& value)
 	{
-		expandBuffer(_size + 1);
-		buffer[_size] = value;
-		return buffer[_size++];
+		expandBuffer(bufferSize + 1);
+		buffer[bufferSize] = value;
+		return buffer[bufferSize++];
 	}
 	inline Type popBack()
 	{
-		if (_size)
-			return buffer[--_size];
+		if (bufferSize)
+			return buffer[--bufferSize];
 		else
 			throw;
 	}
-	inline Type& back() { return buffer[_size - 1]; }
+	inline Type& back() { return buffer[bufferSize - 1]; }
 
 	template <typename KeyType>
 	Type* find(const KeyType& key)
 	{
-		for (Type *current = buffer, *end = buffer + _size; current < end; current++)
+		for (Type *current = buffer, *end = buffer + bufferSize; current < end; current++)
 		{
 			if (*current == key)
 				return current;
@@ -70,7 +70,7 @@ public:
 	template <typename KeyType>
 	uint32 findIndex(const KeyType& key)
 	{
-		for (uint32 i = 0; i < _size; i++)
+		for (uint32 i = 0; i < bufferSize; i++)
 		{
 			if (buffer[i] == key)
 				return i;
@@ -80,32 +80,32 @@ public:
 
 	void resize(uint32 newSize)
 	{
-		_size = newSize;
-		expandBuffer(_size);
+		bufferSize = newSize;
+		expandBuffer(bufferSize);
 	}
 	void compact()
 	{
-		buffer.resize(_size);
+		buffer.resize(bufferSize);
 	}
 
 	Vector<Type> copy()
 	{
-		return Vector<Type>(_size, buffer.getData());
+		return Vector<Type>(bufferSize, buffer.getData());
 	}
 	HeapBuffer<Type> convertToHeapArray()
 	{
-		buffer.resize(_size);
-		_size = 0;
+		buffer.resize(bufferSize);
+		bufferSize = 0;
 		return move(buffer);
 	}
 
 	inline operator Type*() const { return buffer; }
-	inline uint32 size() const { return _size; }
-	inline uint32 byteSize() const { return _size * sizeof(Type); }
-	inline bool isEmpty() const { return _size ? false : true; }
+	inline uint32 size() const { return bufferSize; }
+	inline uint32 byteSize() const { return bufferSize * sizeof(Type); }
+	inline bool isEmpty() const { return bufferSize ? false : true; }
 
 	inline Type* begin() const { return buffer; }
-	inline Type* end() const { return buffer + _size; }
+	inline Type* end() const { return buffer + bufferSize; }
 
 	static constexpr uint32 indexNotFound = uint32(-1);
 };
