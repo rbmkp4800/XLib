abe1ec548c5917719285af39e5137059bc1c734a
rbmkp4800 <rbmkp4800@gmail.com>
2017-09-16 23:04:21 +0300

    Strings.Parse defined

--- Source/XLib.Strings.h
+++ Source/XLib.Strings.h
@@ -39,5 +39,43 @@ namespace XLib
 		
 		//template <uintptr length> static constexpr uintptr Length(const char(&)[length]) { return length - 1; }
 		//template <uintptr length> static constexpr uintptr Length(const wchar(&)[length]) { return length - 1; }
+
+		template <typename Type>
+		static inline bool Parse(const char* string, Type& value, const char** end = nullptr) { static_assert("invalid type"); }
+
+		template <>
+		static inline bool Parse<sint32>(const char* string, sint32& value, const char** end)
+		{
+			bool minus = false;
+			char c = *string;
+			if (c >= '0' && c <= '9')
+				goto label_parsingDigits;
+			
+			if (c == '-')
+				minus = true;
+			else if (c != '+')
+				return nullptr;
+
+			string++;
+			c = *string;
+
+		label_parsingDigits:
+			sint32 result = c - '0';
+			string++;
+			c = *string;
+			while (c >= '0' && c <= '9')
+			{
+				result *= 10;
+				result += c - '0';
+
+				string++;
+				c = *string;
+			}
+
+			value = minus ? -result : result;
+			if (end)
+				*end = string;
+			return true;
+		}
 	};
 }
